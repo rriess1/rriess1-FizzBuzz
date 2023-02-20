5e4248322cdc4f6ced7997ce41498511f0b24308
rriess1 <robertwriess@yahoo.com>
2023-02-20 18:42:23 +0000

    first

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,15 @@ public class Lab {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
+        if(n % 5 == 0 && n % 3 == 0){
+            return "FizzBuzz";
+        }
+        if(n % 3 == 0){
+            return "Fizz";
+        }
+        if(n % 5 == 0){
+            return "Buzz";
+        }
         return "";
     }
 }
